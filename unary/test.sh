3eeb1e8f3fcf29e9a98ef762db524ec5af173963
nissy-shota <shotagtr32@gmail.com>
2020-08-20 01:47:03 +0900

    add test.sh shell

---
+++ unary/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
+assert 10 '-10+20'
+assert 10 '- -10'
+assert 10 '- - +10'
+
+echo OK
