fa7ef27cc836e59144c19f598dc931fdaf159535
nissy-shota <shotagtr32@gmail.com>
2020-08-31 21:36:51 +0900

    define code gen

---
+++ shocc/codegen.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include "shocc.h"
+
+void gen_val(Node *node) {
+    if (node->kind != ND_VAR)
+        error("代入の左辺値が変数ではありません");
+    int offset = (node->name - 'a' + 1) * 8;
+    printf("  mov rax, rbp\n");
+    printf("  sub rax, %d\n", offset);
+    printf("  push rax\n");
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %ld\n", node->val);
+        return;
+    switch(node->kind) {
+        case ND_NUM:
+            printf("  push %ld\n", node->val);
+            return;
+        case ND_VAR:
+            gen_val(node);
+            printf("  pop rax\n");
+            printf("  mov rax, [rax]\n");
+            printf("  push rax\n");
+            return;
+        case ND_ASSIGN:
+            gen_val(node->lhs);
+            gen(node->rhs);
+            printf("  pop rdi\n");
+            printf("  pop rax\n");
+            printf("  mov [rax], rdi\n");
+            printf("  push rdi\n");
+            return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+    switch(node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+        case ND_EQ:
+            printf("  setne al\n");
+            printf("  movzb rax, al\n");
+            break;
+        case ND_LE:
+            printf("  cmp rax, rdi\n");
+            printf("  setle al\n");
+            printf("  movzb rax, al\n");
+            break;
+        case ND_LT:
+            printf("  cmp rax, rdi\n");
+            printf("  setl al\n");
+            printf("  movzb rax, al\n");
+            break;
+    }
+    printf("  push rax\n");
+}
+void codegen(Node *node) {
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+
+    // prologue
+    printf("  push rbp\n");
+    printf("  mov rbp, rsp\n");
+    printf("  sub rsp, 208\n");
+
+    for (Node *n = node; n; n = n->next) {
+        gen(n);
+        printf("  pop rax\n");
+    }
+
+    // epilogue
+    printf("  mov rsp, rbp\n");
+    printf("  pop rbp\n");
+    printf("  ret\n");
+}
